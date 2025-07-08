cf66898efe4db934b5ef2b5bed07b104b9d9fb3f
Uros Tomic <tomicuros006@gmail.com>
2025-07-08 22:58:24 +0200

    Prvi commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the website of a Bachelor Applied Engineer in Information Technology and Systems. The site is currently under construction.</h1>
+      <p>
+        If you would like to get in touch, feel free to contact me at{' '}
+        <a href="mailto:tomicuros006@gmail.com">mail</a>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
